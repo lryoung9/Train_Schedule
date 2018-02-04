4ba72b5a7e811c9c91dfabc39a75c50d9a8cf276
Lydia Young <lydiaryoung@gmail.com>
2018-02-03 23:52:04 -0700

    initialized firebase configuration

---
+++ functionality.js
@@ -0,0 +1,17 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAd6D-WiMycjXeJuKWIJZMBK5dTnLwx3GY",
+    authDomain: "train-schedule-c6354.firebaseapp.com",
+    databaseURL: "https://train-schedule-c6354.firebaseio.com",
+    projectId: "train-schedule-c6354",
+    storageBucket: "train-schedule-c6354.appspot.com",
+    messagingSenderId: "10612507847"
+};
+
+firebase.initializeApp(config);
+
+// Variables
+var name;
+var destination;
+var first-train;
+var frequency;
