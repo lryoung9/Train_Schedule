41458a16319f5bbad7a9df298a0b9c4c8d9e09a7
Lydia Young <lydiaryoung@gmail.com>
2018-02-05 23:32:47 -0700

    time until next train calculated

--- functionality.js
+++ functionality.js
@@ -27,18 +27,11 @@ $("#add-train-btn").on("click", function(event){
 		firstTrain: moment($("#train-time-input").val().trim(), "HH:mm").format("HH:mm"),
 		frequency: parseInt($("#frequency-input").val().trim())
 	};
+	// Test console
 	console.log(newTrain)
-
-	// console.log(name);
-	// console.log(destination);
-	// console.log(firstTrain.format("HH:mm"));
-	// console.log(frequency);
 
 	// Upload new train data to database
 	database.ref().push(newTrain);
-
-	// Test console
-	console.log(database.ref());
 
 	// Clear text-input boxes
 	$("#name-input").val("");
@@ -48,27 +41,44 @@ $("#add-train-btn").on("click", function(event){
 });
 
 database.ref().on("child_added", function(childSnapshot) {
- console.log(childSnapshot.val().name);
- console.log(childSnapshot.val().destination);
- console.log(childSnapshot.val().first-train);
- console.log(childSnapshot.val().frequency);
+	console.log(childSnapshot.val().name);
+	console.log(childSnapshot.val().destination);
+	console.log(childSnapshot.val().firstTrain);
+	console.log(childSnapshot.val().frequency);
+	firstTrain = childSnapshot.val().firstTrain;
+	frequency = childSnapshot.val().frequency
 
-// temporary variable for where to add new train info
-var addTrain = $("#add-train");
-// temporary variable to store added train info
-var trainData = "<tr>";
-trainData += "<td>" + childSnapshot.val().name + "</td>";
-trainData += "<td>" + childSnapshot.val().destination + "</td>";
-trainData += "<td>" + childSnapshot.val().first-train + "</td>";
-trainData += "<td>" + moment().diff(first-train, "minutes") + "</td>";
-trainData += "<td>" + childSnapshot.val().frequency + "</td>";
-trainData += "<td></td>";
-trainData += "</tr>";
-addTrain.append(trainData);
+	// temporary variable for where to add new train info
+	var addTrain = $("#add-train");
+	// temporary variable to store added train info
+	var trainData = "<tr>";
+	// name
+	trainData += "<td>" + childSnapshot.val().name + "</td>";
+	// destination
+	trainData += "<td>" + childSnapshot.val().destination + "</td>";
+	// frequency
+	trainData += "<td>" + childSnapshot.val().frequency + "</td>";
+	// push time back 24hrs to make sure it comes before current time
+	var timeConverted = moment(firstTrain, "HH:mm").subtract(1, "day");
+	// current time grab
+	var currentTime = moment();
+	// difference between the times in minutes
+	var timeDifference = moment().diff(timeConverted, "minutes");
+	
+	// time remaining until next train
+	var timeRemaining = parseInt(frequency - (timeDifference % frequency));
+
+	console.log('time remaining: ' + timeRemaining);
+	// next arrival
+	trainData += "<td>" + currentTime.add(timeRemaining).format("hh:mm a") + "</td>";
+
+	// min away
+	trainData += "<td>" + timeRemaining + "</td>";
+
+	addTrain.append(trainData);
 },
 
 // function to catch errors
 function(errorObject) {
    console.log("Errors handled: " + errorObject.code);
 });
-
